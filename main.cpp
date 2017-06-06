fb2f4991a99c8436443627802701aac8e0a33ca4
hdemarco4 <hdemarco@msudenver.edu>
2017-06-06 15:26:16 -0600

    added the code from the assignment

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <unistd.h>
 
 using namespace std;
 
@@ -12,15 +13,28 @@ int main(int arge, char** argv)
     int f;
     int status;
 
+    struct sigaction *action = new (struct sigaction);
+    action->sa_handler = handler;
+    sigemptyset (&(action->sa_mask));
+    assert (sigaction (signum, action, NULL) == 0);
+
     if ((f = fork()) < 0){
         perror("Error");
     }
     else if (f == 0){
-        
+        execl("./child", "child", (char*)NULL);
+     
     }
     else{
         waitpid(f, &status, 0);
     }
 
+
+}
+
+
+int child()
+{
+    
 
 }
