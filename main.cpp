3672ecc487df5a9722f2031a36513289a86c2056
hdemarco <hdemarco@msudenver.edu>
2017-06-09 13:08:46 -0600

    fixed kills and asserts, seems to be working

--- main.cpp
+++ main.cpp
@@ -24,19 +24,19 @@ int main(int arge, char** argv)
     struct sigaction *a1 = new (struct sigaction);
     a1->sa_handler = handler;
     sigemptyset (&(a1->sa_mask));
-    assert (sigaction (signum, a1, NULL) == 0);
+    assert (sigaction (SIGHUP, a1, NULL) == 0);
     sigaction(SIGHUP, a1, NULL);
 
     struct sigaction *a2 = new (struct sigaction);
     a2->sa_handler = handler;
     sigemptyset (&(a2->sa_mask));
-    assert (sigaction (signum, a2, NULL) == 0);
+    assert (sigaction (SIGUSR1, a2, NULL) == 0);
     sigaction(SIGUSR1, a2, NULL);
 
     struct sigaction *a3 = new (struct sigaction);
     a3->sa_handler = handler;
     sigemptyset (&(a3->sa_mask));
-    assert (sigaction (signum, a3, NULL) == 0);
+    assert (sigaction (SIGIO, a3, NULL) == 0);
     sigaction(SIGIO, a3, NULL);
 
     if ((f = fork()) < 0){
@@ -45,12 +45,12 @@ int main(int arge, char** argv)
     else if (f == 0){
         pidc = getpid();
 
-        kill(pidc, 1);
-        kill(pidc, 10);
-        kill(pidc, 29);
-        kill(pidc, 29);
-        kill(pidc, 29);
- 
+        kill(pidc, SIGHUP);
+        kill(pidc, SIGUSR1);
+        kill(pidc, SIGIO);
+        kill(pidc, SIGIO);
+        kill(pidc, SIGIO);
+
     }
     else{
         
@@ -62,5 +62,5 @@ int main(int arge, char** argv)
             cout << "Child didn't exit\n";
     }
 
-
+    return 0;
 }
