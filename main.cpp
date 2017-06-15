f256fee1434fc94a20fd87450905029c408883eb
hdemarco4 <hdemarco@msudenver.edu>
2017-06-15 14:42:02 -0600

    added deletes, ran valgrind: fixed errors

--- main.cpp
+++ main.cpp
@@ -30,23 +30,25 @@ int main(int arge, char** argv)
     int status;
     int pidp = getpid();
     int pidc;
-    int signum;
 
     struct sigaction *a1 = new (struct sigaction);
     a1->sa_handler = handler;
     sigemptyset (&(a1->sa_mask));
+    a1->sa_flags = 0;
     assert (sigaction (SIGHUP, a1, NULL) == 0);
     sigaction(SIGHUP, a1, NULL);
 
     struct sigaction *a2 = new (struct sigaction);
     a2->sa_handler = handler;
     sigemptyset (&(a2->sa_mask));
+    a2->sa_flags = 0;
     assert (sigaction (SIGUSR1, a2, NULL) == 0);
     sigaction(SIGUSR1, a2, NULL);
 
     struct sigaction *a3 = new (struct sigaction);
     a3->sa_handler = handler;
     sigemptyset (&(a3->sa_mask));
+    a3->sa_flags = 0;
     assert (sigaction (SIGIO, a3, NULL) == 0);
     sigaction(SIGIO, a3, NULL);
 
@@ -72,6 +74,10 @@ int main(int arge, char** argv)
         else
             cout << "Child didn't exit\n";
     }
+
+    delete a1;
+    delete a2;
+    delete a3;
 
     return 0;
 }
